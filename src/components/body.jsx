77707132d3c4f10f53ee776aa5b84990cbb0ca16
KWilson718 <wilsonk18@spu.edu>
2025-01-16 18:12:05 -0800

    working on pulling in the correct data to the body component

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,9 +1,23 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { useStore } from "../store/useStore";
 
 export default function Body() {
+    const {state} = useStore();
+
+    const pageTitles = {
+        0: "Home",
+        1: "About",
+        2: "Experience",
+        3: "Projects",
+        4: "Misc",
+    }
+
+    const currentPage = pageTitles[state.page] || "Undefined Page";
+
     return (
         <Box className="body" >
             <h1>This is the Body</h1>
+            <Typography variant="h4">Current Page: {currentPage}</Typography>
         </Box>
     );
 }
